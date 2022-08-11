5518a6834d7c36843e8d54f6e3995f482d2730c6
Davide Toniatti <davide.toniatti@students.uniroma2.eu>
2022-08-11 19:13:13 +0200

    new

--- .config/polybar/scripts/gpu-temp.sh
+++ .config/polybar/scripts/gpu-temp.sh
@@ -4,7 +4,7 @@ status=$(cat /sys/bus/pci/devices/0000:00:01.0/power/runtime_status)
 
 if [ "$status" = 'active' ]; then
     gputemp="$(nvidia-smi --format=nounits,csv,noheader --query-gpu=temperature.gpu)"
-    echo "GPU: $gputemp°C "
+    echo "$gputemp°C "
 else
     echo ""
 fi
